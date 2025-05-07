07cc7a18650b8e95a693dca9146eba813a154425
fatehhundal <fshundal@hotmail.com>
2025-05-07 18:41:19 +0800

    fibonacci_series

---
+++ fibonacci_series.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+    
+num_terms = int(input("Enter the number of terms: "))
+for i in range(num_terms):
+    print(fibonacci(i), end=" ")
